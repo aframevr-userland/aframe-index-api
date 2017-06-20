c7b9cb63da0b3d8bd2786361c548ea75cb7bdd55
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-06-20 01:33:59 -0700

    update list of manifest URLs to preload-submit

--- preload.sh
+++ preload.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Submit manifest for `A-Painter`.
-curl -X POST -H "Content-Type: application/json" -d '{"url": "https://bb8.surge.sh/manifest.json"}' "http://0.0.0.0:3000/api/manifests"
-
-# Submit manifest for `BB8`.
 curl -X POST -H "Content-Type: application/json" -d '{"url": "https://aframe.io/a-painter"}' "http://0.0.0.0:3000/api/manifests"
+
+# Submit manifest for `A-Saturday-Night`.
+curl -X POST -H "Content-Type: application/json" -d '{"url": "https://aframe.io/a-saturday-night/"}' "http://0.0.0.0:3000/api/manifests"
